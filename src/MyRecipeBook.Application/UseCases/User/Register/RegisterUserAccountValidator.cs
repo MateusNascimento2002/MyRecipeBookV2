6a9b1fc7d19a539104cf4a7c36184cf75e8b783d
Mateus Nascimento <mateus.fnascimento1006@gmail.com>
2026-06-01 21:50:32 -0300

    Start User register use case

---
+++ src/MyRecipeBook.Application/UseCases/User/Register/RegisterUserAccountValidator.cs
@@ -0,0 +1,29 @@
+﻿using FluentValidation;
+using MyRecipeBook.Communication.Requests;
+
+namespace MyRecipeBook.Application.UseCases.User.Register;
+
+public class RegisterUserAccountValidator : AbstractValidator<RequestRegisterUserAccountJson>
+{
+    public RegisterUserAccountValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .WithMessage("Name is required.");
+
+        RuleFor(x => x.Email)
+            .NotEmpty()
+            .WithMessage("Email is required.");
+
+        RuleFor(x => x.Password)
+            .NotEmpty()
+            .MinimumLength(8);
+
+        When(user => !string.IsNullOrEmpty(user.Email), () =>
+        {
+            RuleFor(x => x.Email) 
+                .EmailAddress()
+                .WithMessage("Invalid email format.");
+        });
+    }
+}
