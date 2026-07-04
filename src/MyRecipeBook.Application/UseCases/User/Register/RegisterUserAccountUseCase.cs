46f9e93dbfe680843e4017078d6034d2590e15a4
Mateus Nascimento <mateus.fnascimento1006@gmail.com>
2026-07-03 22:06:48 -0300

    Create user entitie

--- src/MyRecipeBook.Application/UseCases/User/Register/RegisterUserAccountUseCase.cs
+++ src/MyRecipeBook.Application/UseCases/User/Register/RegisterUserAccountUseCase.cs
@@ -1,5 +1,6 @@
 ﻿using MyRecipeBook.Communication.Requests;
 using MyRecipeBook.Exception.ExceptionBase;
+using DomainUser = MyRecipeBook.Domain.Entities.User;
 
 namespace MyRecipeBook.Application.UseCases.User.Register;
 
@@ -7,13 +8,19 @@ public class RegisterUserAccountUseCase
 {
     public void Execute(RequestRegisterUserAccountJson request)
     {
+        ValidateAndThrowOnFailure(request);
+        
+        var user = DomainUser.Create(request.Name, request.Email, request.Password);
+    }
+
+    private void ValidateAndThrowOnFailure(RequestRegisterUserAccountJson request)
+    {
         var validator = new RegisterUserAccountValidator();
         var result = validator.Validate(request);
 
-        if (!result.IsValid)
-        {
-            var errorMessages =  result.Errors.Select(e => e.ErrorMessage).ToList();
-            throw new ErrorOnValidationException(errorMessages);
-        }
+        if (result.IsValid) return;
+
+        var errorMessages = result.Errors.Select(e => e.ErrorMessage).ToList();
+        throw new ErrorOnValidationException(errorMessages);
     }
 }
---
+++ src/MyRecipeBook.Domain/Interfaces/IHaveId.cs
@@ -0,0 +1,6 @@
+﻿namespace MyRecipeBook.Domain.Interfaces;
+
+public interface IHaveId
+{
+    public Guid Id { get; }
+}
