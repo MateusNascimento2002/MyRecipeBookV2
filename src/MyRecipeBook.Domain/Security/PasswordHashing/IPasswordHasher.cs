83921c2ad645468fc9e62e366910b714b83f744e
Mateus Nascimento <mateus.fnascimento1006@gmail.com>
2026-07-06 20:15:30 -0300

    Create a password hasher using Argon2

---
+++ src/MyRecipeBook.Domain/Security/PasswordHashing/IPasswordHasher.cs
@@ -0,0 +1,7 @@
+﻿namespace MyRecipeBook.Domain.Security.PasswordHashing;
+
+public interface IPasswordHasher
+{
+    string HashPassword(string password);
+    bool VerifyHashedPassword(string password, string hashedPassword);
+}
