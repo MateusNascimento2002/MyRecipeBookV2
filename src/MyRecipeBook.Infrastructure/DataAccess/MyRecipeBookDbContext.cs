681966abc02beaa62298f579977d86a5018ab295
Mateus Nascimento <mateus.fnascimento1006@gmail.com>
2026-07-06 21:08:20 -0300

    Add DbContext

---
+++ src/MyRecipeBook.Infrastructure/DataAccess/MyRecipeBookDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyRecipeBook.Infrastructure.DataAccess.DatabaseModels;
+
+namespace MyRecipeBook.Infrastructure.DataAccess;
+
+internal class MyRecipeBookDbContext : DbContext
+{
+    public MyRecipeBookDbContext(DbContextOptions<MyRecipeBookDbContext> options) : base(options) { }
+    
+    public DbSet<DatabaseUser> Users { get; set; }
+}
