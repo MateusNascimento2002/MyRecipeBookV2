83345be278e64d9b8f591b61ae7a8f54be138366
Mateus Nascimento <mateus.fnascimento1006@gmail.com>
2026-07-09 16:18:14 -0300

    Create test class to RegisterUserAccountUseCase

---
+++ tests/UseCases.Tests/User/Register/RegisterUserAccountUseCaseTests.cs
@@ -0,0 +1,25 @@
+﻿using CommonTestUtilities.Repositories;
+using CommonTestUtilities.Requests;
+using MyRecipeBook.Application.UseCases.User.Register;
+
+namespace UseCases.Tests.User.Register;
+
+public class RegisterUserAccountUseCaseTests
+{
+    [Fact]
+    public async Task Success()
+    {
+        var request = RequestRegisterUserAccountJsonBuilder.Build();
+        
+    }
+
+    private RegisterUserAccountUseCase CreateUseCase()
+    {
+        var unitOfWork = IUnitOfWorkBuilder.Build();
+        var userWriteOnlyRepository = IUserWriteOnlyRepositoryBuilder.Build();
+        var userReadOnlyRepository = new IUserReadOnlyRepositoryBuilder().Build();
+        var passwordHasher = new IPasswordHasherBuilder().Build();
+        
+        return new RegisterUserAccountUseCase(passwordHasher, userWriteOnlyRepository, userReadOnlyRepository, unitOfWork);
+    }
+}
