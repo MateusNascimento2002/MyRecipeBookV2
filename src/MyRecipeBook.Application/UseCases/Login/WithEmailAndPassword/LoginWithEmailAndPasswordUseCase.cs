cc896391c9cead2d34a9bdc468675db3539aef57
Mateus Nascimento <mateus.fnascimento1006@gmail.com>
2026-07-20 18:15:22 -0300

    Create LoginWithEmailAndPasswordUseCase

---
+++ src/MyRecipeBook.Application/UseCases/Login/WithEmailAndPassword/LoginWithEmailAndPasswordUseCase.cs
@@ -0,0 +1,27 @@
+﻿using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Communication.Responses;
+using MyRecipeBook.Domain.Interfaces.Repositories.Users;
+using MyRecipeBook.Domain.Security.PasswordHashing;
+
+namespace MyRecipeBook.Application.UseCases.Login.WithEmailAndPassword;
+
+public class LoginWithEmailAndPasswordUseCase : ILoginWithEmailAndPasswordUseCase
+{
+    private readonly IPasswordHasher _passwordHasher;
+    private IUserReadOnlyRepository _userReadOnlyRepository;
+
+    public LoginWithEmailAndPasswordUseCase(IPasswordHasher passwordHasher, IUserReadOnlyRepository userReadOnlyRepository)
+    {
+        _passwordHasher = passwordHasher;
+        _userReadOnlyRepository = userReadOnlyRepository;
+    }
+    
+    public async Task<ResponseRegisterUserJson> Execute(RequestLoginJson request)
+    {
+        var user = await _userReadOnlyRepository.GetByEmail(request.Email);
+
+        var isPasswordValid = _passwordHasher.VerifyHashedPassword(request.Password, user.Password);
+
+        return new ResponseRegisterUserJson(user.Id, user.Name, new ResponseTokensJson("access", "refresh"));
+    }
+}
