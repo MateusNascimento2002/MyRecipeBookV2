45f645f8ea4570cc2a1301d65c07fe9f4acdf9ae
Mateus Nascimento <mateus.fnascimento1006@gmail.com>
2026-07-20 18:15:33 -0300

    DI and controller

---
+++ src/MyRecipeBook.API/Controllers/AuthenticationController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyRecipeBook.Application.UseCases.Login.WithEmailAndPassword;
+using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Communication.Responses;
+
+namespace MyRecipeBook.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AuthenticationController : ControllerBase
+{
+    [HttpPost]
+    [ProducesResponseType(typeof(ResponseRegisterUserJson),StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status401Unauthorized)]
+    public async Task<IActionResult> Login(
+        [FromServices] ILoginWithEmailAndPasswordUseCase useCase,
+        [FromBody] RequestLoginJson request)
+    {
+        var result = await useCase.Execute(request);
+        
+        return Ok(result);
+    }
+}
