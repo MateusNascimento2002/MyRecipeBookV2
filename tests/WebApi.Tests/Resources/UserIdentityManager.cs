4493367bccce84db8abbc288819b15e5833c406f
Mateus Nascimento <mateus.fnascimento1006@gmail.com>
2026-07-21 18:46:31 -0300

    Create UserIdentityManager

---
+++ tests/WebApi.Tests/Resources/UserIdentityManager.cs
@@ -0,0 +1,19 @@
+﻿using DomainUser = MyRecipeBook.Domain.Entities.User;
+namespace WebApi.Tests.Resources;
+
+public class UserIdentityManager
+{
+    private readonly DomainUser _user;
+    private readonly string _password;
+
+    public UserIdentityManager(DomainUser user, string password)
+    {
+        _user = user;
+        _password = password;
+    }
+
+    public Guid GetId() => _user.Id;
+    public string GetName() => _user.Name;
+    public string GetEmail() => _user.Email;
+    public string GetPassword() => _password;
+}
