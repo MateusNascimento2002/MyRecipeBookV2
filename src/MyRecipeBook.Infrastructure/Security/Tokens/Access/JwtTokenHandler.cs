d88ff4f490ecbc9c56efcb40140798e816ac5432
Mateus Nascimento <mateus.fnascimento1006@gmail.com>
2026-07-21 19:43:27 -0300

    Create Jwt handler

---
+++ src/MyRecipeBook.Infrastructure/Security/Tokens/Access/JwtTokenHandler.cs
@@ -0,0 +1,45 @@
+﻿using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.JsonWebTokens;
+using Microsoft.IdentityModel.Tokens;
+using MyRecipeBook.Domain.Entities;
+using MyRecipeBook.Domain.Security.Tokens;
+
+namespace MyRecipeBook.Infrastructure.Security.Tokens;
+
+internal sealed class JwtTokenHandler : IAccessTokenGenerator
+{
+    private readonly uint _expirationTimeInMinutes;
+    private readonly string _signingKey;
+
+    public JwtTokenHandler(uint expirationTimeInMinutes, string signingKey)
+    {
+        _expirationTimeInMinutes = expirationTimeInMinutes;
+        _signingKey = signingKey;
+    }
+    
+    public string Generate(User user)
+    {
+        var claims = new List<Claim>
+        {
+            new (JwtRegisteredClaimNames.Sub, user.Id.ToString())
+        };
+        
+        var tokenDescriptor = new SecurityTokenDescriptor()
+        {
+            Expires = DateTime.UtcNow.AddMinutes(_expirationTimeInMinutes),
+            SigningCredentials = new SigningCredentials(Credentials(), SecurityAlgorithms.HmacSha256),
+            Subject = new ClaimsIdentity(claims)
+        };
+        
+        var handler = new JsonWebTokenHandler();
+        
+        return handler.CreateToken(tokenDescriptor);
+    }
+
+    private SymmetricSecurityKey Credentials()
+    {
+        var keyBytes = Encoding.UTF8.GetBytes(_signingKey);
+        return new SymmetricSecurityKey(keyBytes);
+    }
+}
