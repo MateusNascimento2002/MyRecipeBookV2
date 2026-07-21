df1ef7e9172e2108ec26340c2196e390a37a874f
Mateus Nascimento <mateus.fnascimento1006@gmail.com>
2026-07-21 18:17:54 -0300

    Add expection tests to LoginWithEmailAndPasswordUseCaseTests

--- tests/UseCases.Tests/Login/WithEmailAndPassword/LoginWithEmailAndPasswordUseCaseTests.cs
+++ tests/UseCases.Tests/Login/WithEmailAndPassword/LoginWithEmailAndPasswordUseCaseTests.cs
@@ -1,9 +1,12 @@
-﻿using CommonTestUtilities.Entities;
+﻿using System.Net;
+using CommonTestUtilities.Entities;
 using CommonTestUtilities.Repositories;
 using CommonTestUtilities.Requests;
 using MyRecipeBook.Application.UseCases.Login.WithEmailAndPassword;
 using MyRecipeBook.Communication.Requests;
 using MyRecipeBook.Domain.Extensions;
+using MyRecipeBook.Exception;
+using MyRecipeBook.Exception.ExceptionBase;
 using Shouldly;
 using DomainUser = MyRecipeBook.Domain.Entities.User;
 namespace UseCases.Tests.Login.WithEmailAndPassword;
@@ -27,6 +30,40 @@ public class LoginWithEmailAndPasswordUseCaseTests
         result.Name.ShouldBe(user.Name);
         result.Tokens.AccessToken.ShouldBeNullOrEmpty();
         result.Tokens.RefreshToken.ShouldBeNullOrEmpty();
+    }
+    
+    [Fact]
+    public async Task ShouldThrowException_WhenUserDontExist()
+    {
+        var request = RequestLoginJsonBuilder.Build();
+        
+        var useCase = CreateUseCase();
+
+        var exception = await useCase.Execute(request).ShouldThrowAsync<InvalidLoginException>();
+        
+        exception.GetStatusCode().ShouldBe(HttpStatusCode.Unauthorized);
+        exception.GetErrorMessages().ShouldSatisfyAllConditions(errorMessages =>
+        {
+            errorMessages.Count.ShouldBe(1);
+            errorMessages.ShouldContain(ResourceMessagesException.VALIDATION_LOGIN_INVALID);
+        });
+    }
+    
+    [Fact]
+    public async Task ShouldThrowException_WhenPasswordIsWrong()
+    {
+        var request = RequestLoginJsonBuilder.Build();
+        var user = UserBuilder.Build();
+        var useCase = CreateUseCase(password: string.Empty, user);
+
+        var exception = await useCase.Execute(request).ShouldThrowAsync<InvalidLoginException>();
+        
+        exception.GetStatusCode().ShouldBe(HttpStatusCode.Unauthorized);
+        exception.GetErrorMessages().ShouldSatisfyAllConditions(errorMessages =>
+        {
+            errorMessages.Count.ShouldBe(1);
+            errorMessages.ShouldContain(ResourceMessagesException.VALIDATION_LOGIN_INVALID);
+        });
     }
 
     private LoginWithEmailAndPasswordUseCase CreateUseCase(string? password = null, DomainUser? user = null)
