7326f11c304656db71ee254ec4eb2d945820cfff
Mateus Nascimento <mateus.fnascimento1006@gmail.com>
2026-07-23 18:19:42 -0300

    Change password adjust

--- src/MyRecipeBook.Application/UseCases/User/ChangePassword/ChangePasswordUseCase.cs
+++ src/MyRecipeBook.Application/UseCases/User/ChangePassword/ChangePasswordUseCase.cs
@@ -1,6 +1,7 @@
 ﻿using FluentValidation.Results;
 using MyRecipeBook.Communication.Requests;
 using MyRecipeBook.Domain.Identity;
+using MyRecipeBook.Domain.Interfaces.Repositories.Users;
 using MyRecipeBook.Domain.Security.PasswordHashing;
 using MyRecipeBook.Exception;
 using MyRecipeBook.Exception.ExceptionBase;
@@ -11,18 +12,21 @@ public class ChangePasswordUseCase : IChangePasswordUseCase
 {
     private readonly ILoggedUser _loggedUser;
     private readonly IPasswordHasher _passwordHasher;
+    private readonly IUserUpdateOnlyRepository _userUpdateOnlyRepository;
 
-    public ChangePasswordUseCase(ILoggedUser loggedUser, IPasswordHasher passwordHasher)
+    public ChangePasswordUseCase(ILoggedUser loggedUser, IPasswordHasher passwordHasher,  IUserUpdateOnlyRepository userUpdateOnlyRepository)
     {
         _loggedUser = loggedUser;
         _passwordHasher = passwordHasher;
+        _userUpdateOnlyRepository = userUpdateOnlyRepository;
     }
     
     public async Task Execute(RequestChangePasswordJson request)
     {
         var user = await _loggedUser.Get();
         Validate(request, user);
-        user.Password = _passwordHasher.HashPassword(request.NewPassword);
+        var hashedPassword = _passwordHasher.HashPassword(request.NewPassword);
+        await _userUpdateOnlyRepository.UpdatePassword(user.Id, hashedPassword);
     }
 
     private void Validate(RequestChangePasswordJson request, DomainUser user)
