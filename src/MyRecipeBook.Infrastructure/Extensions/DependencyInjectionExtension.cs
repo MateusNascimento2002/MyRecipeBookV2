b35b29fb60673dac9b8ebc53e563c1b588c35319
Mateus Nascimento <mateus.fnascimento1006@gmail.com>
2026-07-23 18:19:51 -0300

    DI

--- src/MyRecipeBook.Infrastructure/Extensions/DependencyInjectionExtension.cs
+++ src/MyRecipeBook.Infrastructure/Extensions/DependencyInjectionExtension.cs
@@ -37,6 +37,7 @@ public static class DependencyInjectionExtension
         {
             services.AddScoped<IUserWriteOnlyRepository, UserRepository>();
             services.AddScoped<IUserReadOnlyRepository, UserRepository>();
+            services.AddScoped<IUserUpdateOnlyRepository, UserRepository>();
         }
 
         private void AddUnitOfWork()
