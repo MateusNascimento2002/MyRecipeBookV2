3d719ccf794d550a207ec290397226646e4ccb84
Mateus Nascimento <mateus.fnascimento1006@gmail.com>
2026-07-27 19:59:27 -0300

    Add controller and use case to register recipe

---
+++ src/MyRecipeBook.API/Controllers/RecipeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MyRecipeBook.Application.UseCases.Recipe.Register;
+using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Communication.Responses;
+
+namespace MyRecipeBook.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+[Authorize]
+public class RecipesController : ControllerBase
+{
+    [HttpPost]
+    [ProducesResponseType(typeof(ResponseRegisteredRecipeJson), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(ResponseErrorJson), StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> Register([FromServices] IRegisterRecipeUseCase  useCase, [FromBody] RequestRecipeJson request)
+    {
+        var result = await  useCase.Execute(request);
+        return Created(string.Empty, result);
+    }
+}
---
+++ src/MyRecipeBook.Application/UseCases/Recipe/Register/RegisterRecipeUseCase.cs
@@ -0,0 +1,48 @@
+﻿using Mapster;
+using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Communication.Responses;
+using MyRecipeBook.Domain.Identity;
+using MyRecipeBook.Domain.Interfaces.Repositories.Recipe;
+using MyRecipeBook.Domain.Interfaces.Repositories.UnitOfWork;
+using MyRecipeBook.Exception.ExceptionBase;
+using DomainRecipe = MyRecipeBook.Domain.Entities.Recipe;
+namespace MyRecipeBook.Application.UseCases.Recipe.Register;
+
+public class RegisterRecipeUseCase : IRegisterRecipeUseCase
+{
+    private readonly IRecipeWriteOnlyRepository  _recipeWriteOnlyRepository;
+    private readonly IUnitOfWork  _unitOfWork;
+    private readonly ILoggedUser  _loggedUser;
+
+    public RegisterRecipeUseCase(IRecipeWriteOnlyRepository recipeWriteOnlyRepository, IUnitOfWork unitOfWork, ILoggedUser loggedUser)
+    {
+        _recipeWriteOnlyRepository = recipeWriteOnlyRepository;
+        _unitOfWork = unitOfWork;
+        _loggedUser = loggedUser;
+    }
+    
+    public async Task<ResponseRegisteredRecipeJson> Execute(RequestRecipeJson request)
+    {
+        Validate(request);
+        
+        var recipe = request.Adapt<DomainRecipe>();
+        
+        recipe.UserId = _loggedUser.GetUserId();
+        
+        await _recipeWriteOnlyRepository.Add(recipe);
+        
+        await _unitOfWork.CommitAsync();
+        
+        return recipe.Adapt<ResponseRegisteredRecipeJson>();
+    }
+
+    private void Validate(RequestRecipeJson request)
+    {
+        var result = new RecipeValidator().Validate(request);
+
+        if (result.IsValid == false)
+        {
+            throw new ErrorOnValidationException(result.Errors.Select(e => e.ErrorMessage).ToList());
+        }
+    }
+}
