10137e5da8158d9714d4162f84387ecae0ce3438
Mateus Nascimento <mateus.fnascimento1006@gmail.com>
2026-07-28 17:26:26 -0300

    Create controller

--- src/MyRecipeBook.API/Controllers/RecipeController.cs
+++ src/MyRecipeBook.API/Controllers/RecipeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using MyRecipeBook.Application.UseCases.Recipe.GetById;
 using MyRecipeBook.Application.UseCases.Recipe.Register;
 using MyRecipeBook.Communication.Requests;
 using MyRecipeBook.Communication.Responses;
@@ -18,5 +19,14 @@ public class RecipesController : ControllerBase
     {
         var result = await  useCase.Execute(request);
         return Created(string.Empty, result);
+    }
+    
+    [HttpGet("{id:guid}")]
+    [ProducesResponseType(typeof(ResponseRecipeJson), StatusCodes.Status200OK)]
+    [ProducesResponseType( typeof(ResponseErrorJson), StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> GetById([FromServices] IGetRecipeByIdUseCase useCase, [FromRoute] Guid id)
+    {
+        var result = await useCase.Execute(id);
+        return Ok(result);
     }
 }
