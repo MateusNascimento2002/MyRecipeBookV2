ed5893105e443773ff84ede941f03162060bd0a9
Mateus Nascimento <mateus.fnascimento1006@gmail.com>
2026-07-28 17:45:02 -0300

    Get recipe by id use case tests

---
+++ tests/CommonTestUtilities/Repositories/Recipe/IRecipeReadOnlyRepositoryBuilder.cs
@@ -0,0 +1,22 @@
+﻿using Moq;
+using MyRecipeBook.Domain.Interfaces.Repositories.Recipe;
+
+namespace CommonTestUtilities.Repositories.Recipe;
+
+public class IRecipeReadOnlyRepositoryBuilder
+{
+    private readonly Mock<IRecipeReadOnlyRepository> _recipeReadOnlyRepositoryMock;
+
+    public IRecipeReadOnlyRepositoryBuilder()
+    {
+        _recipeReadOnlyRepositoryMock = new Mock<IRecipeReadOnlyRepository>();
+    }
+
+    public IRecipeReadOnlyRepositoryBuilder GetById(MyRecipeBook.Domain.Entities.Recipe recipe)
+    {
+        _recipeReadOnlyRepositoryMock.Setup(repo => repo.GetById(recipe.Id, recipe.UserId)).ReturnsAsync(recipe);
+        return this;
+    }
+
+    public IRecipeReadOnlyRepository Build() => _recipeReadOnlyRepositoryMock.Object;
+}
---
+++ tests/UseCases.Tests/Recipe/GetById/GetRecipeByIdUseCaseTests.cs
@@ -0,0 +1,58 @@
+﻿using System.Net;
+using CommonTestUtilities.Entities;
+using CommonTestUtilities.Identity;
+using CommonTestUtilities.Repositories.Recipe;
+using MyRecipeBook.Application.Mappings;
+using MyRecipeBook.Application.UseCases.Recipe.GetById;
+using MyRecipeBook.Communication.Responses;
+using MyRecipeBook.Exception;
+using MyRecipeBook.Exception.ExceptionBase;
+using Shouldly;
+
+namespace UseCases.Tests.Recipe.GetById;
+
+public class GetRecipeByIdUseCaseTests
+{
+    [Fact]
+    public async Task Success()
+    {
+        var (user, _) = UserBuilder.Build();
+        var recipe = RecipeBuilder.Build(user);
+        
+        var useCase = CreateUseCase(recipe, user);
+        
+        var result = await useCase.Execute(recipe.Id);
+
+        result.ShouldNotBeNull();
+        result.Id.ShouldBe(recipe.Id);
+        result.Title.ShouldBe(recipe.Title);
+        
+        result.Instructions.Select(c => c.Order).ShouldBeInOrder(SortDirection.Ascending);
+    }
+
+    [Fact]
+    public async Task Validate_ShouldThrowException_WhenRecipeNotFound()
+    {
+        var (user, _) = UserBuilder.Build();
+        var recipe = RecipeBuilder.Build(user);
+        
+        var useCase = CreateUseCase(recipe, user);
+        
+        var exception = await useCase.Execute(Guid.CreateVersion7()).ShouldThrowAsync<NotFoundException>();
+
+        exception.GetStatusCode().ShouldBe(HttpStatusCode.NotFound);
+        exception.GetErrorMessages().ShouldSatisfyAllConditions(errorMessages =>
+        {
+            errorMessages.Count.ShouldBe(1);
+            errorMessages.ShouldContain(ResourceMessagesException.VALIDATION_RECIPE_NOT_FOUND);
+        });
+    }
+    
+    private static GetRecipeByIdUseCase CreateUseCase(MyRecipeBook.Domain.Entities.Recipe recipe, MyRecipeBook.Domain.Entities.User user)
+    {
+        MapsterConfiguration.Configure();
+        var loggedUser = ILoggedUserBuilder.Build(user);
+        var repository = new IRecipeReadOnlyRepositoryBuilder().GetById(recipe).Build();
+        return new GetRecipeByIdUseCase(repository, loggedUser);
+    }
+}
