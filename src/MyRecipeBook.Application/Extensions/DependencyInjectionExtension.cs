95493b7173c4ae8b4f36017812cf60f7d3234095
Mateus Nascimento <mateus.fnascimento1006@gmail.com>
2026-08-04 19:08:16 -0300

    Create get recents recipes use case

--- src/MyRecipeBook.Application/Extensions/DependencyInjectionExtension.cs
+++ src/MyRecipeBook.Application/Extensions/DependencyInjectionExtension.cs
@@ -3,6 +3,7 @@ using MyRecipeBook.Application.Mappings;
 using MyRecipeBook.Application.UseCases.Login.WithEmailAndPassword;
 using MyRecipeBook.Application.UseCases.Recipe.Delete;
 using MyRecipeBook.Application.UseCases.Recipe.GetById;
+using MyRecipeBook.Application.UseCases.Recipe.GetRecent;
 using MyRecipeBook.Application.UseCases.Recipe.Register;
 using MyRecipeBook.Application.UseCases.Recipe.Update;
 using MyRecipeBook.Application.UseCases.User.ChangePassword;
@@ -28,11 +29,14 @@ public static class DependencyInjectionExtension
             services.AddScoped<IGetUserProfileUseCase, GetUserProfileUseCase>();
             services.AddScoped<IChangePasswordUseCase, ChangePasswordUseCase>();
             services.AddScoped<IUpdateUserUseCase, UpdateUserUseCase>();
+            
             services.AddScoped<ILoginWithEmailAndPasswordUseCase, LoginWithEmailAndPasswordUseCase>();
+            
             services.AddScoped<IRegisterRecipeUseCase, RegisterRecipeUseCase>();
             services.AddScoped<IGetRecipeByIdUseCase, GetRecipeByIdUseCase>();
             services.AddScoped<IDeleteRecipeByIdUseCase, DeleteRecipeByIdUseCase>();
             services.AddScoped<IUpdateRecipeByIdUseCase, UpdateRecipeByIdUseCase>();
+            services.AddScoped<IGetRecentRecipesUseCase, GetRecentRecipesUseCase>();
         }
     }
 }
---
+++ src/MyRecipeBook.Application/UseCases/Recipe/GetRecent/GetRecentRecipesUseCase.cs
@@ -0,0 +1,29 @@
+﻿using Mapster;
+using MyRecipeBook.Communication.Responses;
+using MyRecipeBook.Domain.Identity;
+using MyRecipeBook.Domain.Interfaces.Repositories.Recipe;
+
+namespace MyRecipeBook.Application.UseCases.Recipe.GetRecent;
+
+public class GetRecentRecipesUseCase : IGetRecentRecipesUseCase
+{
+    private readonly IRecipeReadOnlyRepository _recipeReadOnlyRepository;
+    private readonly ILoggedUser _loggedUser;
+
+    public GetRecentRecipesUseCase(IRecipeReadOnlyRepository recipeReadOnlyRepository, ILoggedUser loggedUser)
+    {
+        _recipeReadOnlyRepository = recipeReadOnlyRepository;
+        _loggedUser = loggedUser;
+    }
+    
+    public async Task<ResponseRecipesJson> Execute()
+    {
+        var recipes = await _recipeReadOnlyRepository.GetRecentRecipes(_loggedUser.GetUserId());
+        var response = new ResponseRecipesJson()
+        {
+            Recipes = recipes.Adapt<IList<ResponseRecipeSummaryJson>>()
+        };
+
+        return response;
+    }
+}
