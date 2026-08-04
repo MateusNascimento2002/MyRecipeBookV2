7d32e91353148ccd63b364760180a12880db64ce
Mateus Nascimento <mateus.fnascimento1006@gmail.com>
2026-08-04 17:55:55 -0300

    Create delete recipe endpoint

--- src/MyRecipeBook.API/Controllers/RecipeController.cs
+++ src/MyRecipeBook.API/Controllers/RecipeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using MyRecipeBook.Application.UseCases.Recipe.Delete;
 using MyRecipeBook.Application.UseCases.Recipe.GetById;
 using MyRecipeBook.Application.UseCases.Recipe.Register;
 using MyRecipeBook.Communication.Requests;
@@ -28,5 +29,14 @@ public class RecipesController : ControllerBase
     {
         var result = await useCase.Execute(id);
         return Ok(result);
+    }
+    
+    [HttpDelete("{id:guid}")]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType( typeof(ResponseErrorJson), StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> DeleteById([FromServices] IDeleteRecipeByIdUseCase useCase, [FromRoute] Guid id)
+    {
+        await useCase.Execute(id);
+        return NoContent();
     }
 }
