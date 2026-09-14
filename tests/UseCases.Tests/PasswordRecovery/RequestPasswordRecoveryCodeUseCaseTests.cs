6a6a1fa8eccdf5b077dcfd39db7ff4448c0653e1
Mateus Nascimento <mateus.fnascimento1006@gmail.com>
2026-09-14 20:10:02 -0300

    Create verification code use case tests

---
+++ tests/UseCases.Tests/PasswordRecovery/RequestPasswordRecoveryCodeUseCaseTests.cs
@@ -0,0 +1,50 @@
+﻿using CommonTestUtilities.Entities;
+using CommonTestUtilities.Repositories;
+using CommonTestUtilities.Repositories.User;
+using CommonTestUtilities.Repositories.VerificationCode;
+using CommonTestUtilities.Requests;
+using MyRecipeBook.Application.UseCases.PasswordRecovery;
+using MyRecipeBook.Communication.Requests;
+using Shouldly;
+
+namespace UseCases.Tests.PasswordRecovery;
+
+public class RequestPasswordRecoveryCodeUseCaseTests
+{
+    [Fact]
+    public async Task Success_WhenUserExists()
+    {
+        var (user, _) = UserBuilder.Build();
+
+        var request = new RequestPasswordRecoveryJson()
+        {
+            Email = user.Email,
+        };
+        
+        var useCase = CreateUseCase(user);
+
+        await useCase.Execute(request).ShouldNotThrowAsync();
+    }
+    
+    [Fact]
+    public async Task Success_WhenUserDoesNotExists()
+    {
+        var request = RequestPassowrdRecoveryJsonBuilder.Build();
+        
+        var useCase = CreateUseCase();
+
+        await useCase.Execute(request).ShouldNotThrowAsync();
+    }
+    
+    private static RequestPasswordRecoveryCodeUseCase CreateUseCase(MyRecipeBook.Domain.Entities.User? user = null)
+    {
+        var unitOfWork = IUnitOfWorkBuilder.Build();
+        var verificationCodeWriteOnlyRepository = IVerificationCodeWriteOnlyRepositoryBuilder.Build();
+        var userReadonlyRepository = new IUserReadOnlyRepositoryBuilder();
+
+        if (user is not null)
+            userReadonlyRepository.GetUserByEmail(user);
+
+        return new RequestPasswordRecoveryCodeUseCase(userReadonlyRepository.Build(), verificationCodeWriteOnlyRepository, unitOfWork);
+    }
+}
