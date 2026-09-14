0177ddc5be9744d779e29d8275ba68c7d54e3791
Mateus Nascimento <mateus.fnascimento1006@gmail.com>
2026-09-14 20:17:55 -0300

    Create verification code integration tests

---
+++ tests/WebApi.Tests/PasswordRecovery/RequestPasswordRecoveryCodeTests.cs
@@ -0,0 +1,62 @@
+﻿using System.Net;
+using CommonTestUtilities.Requests;
+using Microsoft.EntityFrameworkCore;
+using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Domain.Enums.VerificationCode;
+using Shouldly;
+using WebApi.Tests.Resources;
+
+namespace WebApi.Tests.PasswordRecovery;
+
+public class RequestPasswordRecoveryCodeTests : BaseIntegrationTest
+{
+    private const string REQUEST_URI = "/authentication/password-recovery";
+    private readonly UserIdentityManager _user1;
+
+    public RequestPasswordRecoveryCodeTests(MyRecipeBookApplicationFactory factory) : base(factory)
+    {
+        _user1 = factory.User1;
+    }
+
+    [Fact]
+    public async Task Success_WhenUserExists()
+    {
+        var request = new RequestPasswordRecoveryJson()
+        {
+            Email = _user1.GetEmail()
+        };
+
+        var response = await Post(REQUEST_URI, request);
+
+        response.StatusCode.ShouldBe(HttpStatusCode.Accepted);
+
+        var responseBody = await response.Content.ReadAsStringAsync();
+
+        responseBody.ShouldBeEmpty();
+
+        var codeGenerated = await DbContext.VerificationCodes.AnyAsync(code =>
+            code.UserId == _user1.GetId() && code.Type == VerificationCodeType.PasswordRecovery);
+
+        codeGenerated.ShouldBeTrue();
+    }
+
+    [Fact]
+    public async Task Success_WhenUserDoesNotExists()
+    {
+        var request = RequestPassowrdRecoveryJsonBuilder.Build();
+        
+        var codesBefore = await DbContext.VerificationCodes.AsNoTracking().CountAsync();
+        
+        var response = await Post(REQUEST_URI, request);
+
+        response.StatusCode.ShouldBe(HttpStatusCode.Accepted);
+
+        var responseBody = await response.Content.ReadAsStringAsync();
+
+        responseBody.ShouldBeEmpty();
+
+        var codesAfter = await DbContext.VerificationCodes.AsNoTracking().CountAsync();
+
+        codesAfter.ShouldBe(codesBefore);
+    }
+}
