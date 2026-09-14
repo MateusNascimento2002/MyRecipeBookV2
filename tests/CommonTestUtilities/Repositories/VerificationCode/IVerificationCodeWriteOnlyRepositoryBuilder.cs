2f2772d62f315da5855aa7b994a893f5f2295749
Mateus Nascimento <mateus.fnascimento1006@gmail.com>
2026-09-14 20:09:46 -0300

    Create Verification code common utilities

---
+++ tests/CommonTestUtilities/Repositories/VerificationCode/IVerificationCodeWriteOnlyRepositoryBuilder.cs
@@ -0,0 +1,14 @@
+﻿using Moq;
+using MyRecipeBook.Domain.Interfaces.Repositories.VerificationCode;
+
+namespace CommonTestUtilities.Repositories.VerificationCode;
+
+public class IVerificationCodeWriteOnlyRepositoryBuilder
+{
+    public static IVerificationCodeWriteOnlyRepository Build()
+    {
+        var mock = new Mock<IVerificationCodeWriteOnlyRepository>();
+
+        return mock.Object;
+    }
+}
---
+++ tests/CommonTestUtilities/Requests/RequestPassowrdRecoveryJsonBuilder.cs
@@ -0,0 +1,13 @@
+﻿using Bogus;
+using MyRecipeBook.Communication.Requests;
+
+namespace CommonTestUtilities.Requests;
+
+public class RequestPassowrdRecoveryJsonBuilder
+{
+    public static RequestPasswordRecoveryJson Build()
+    {
+        return new Faker<RequestPasswordRecoveryJson>()
+            .RuleFor(request => request.Email, faker => faker.Internet.Email());
+    }
+}
